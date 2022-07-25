c11f78254a4ae107f7da80301ee494d4b41b7528
Pimenta <mateuspimartins@hotmail.com>
2022-07-25 16:30:45 -0300

    feat: add tests  get/tests

--- tests/testsEntity.test.ts
+++ tests/testsEntity.test.ts
@@ -103,7 +103,7 @@ describe('post /test test suit', () => {
 });
 
 describe('get /tests test suit', () => {
-  it('given groupBy = discipline should return ordened by discipline, expect 200', async () => {
+  it('given groupBy = disciplines should return ordened by discipline, expect 200', async () => {
     const token = UserFactory.generateToken();
     await testFactory.createTestData();
     const response = await supertest(app)
@@ -113,14 +113,34 @@ describe('get /tests test suit', () => {
     expect(response.body.tests.discipline).not.toBe(null);
   });
 
-  it('missing valid token should return 401', async () => {
+  it('given groupBy = teachers should return ordened by teachers, expect 200', async () => {
+    const token = UserFactory.generateToken();
+    await testFactory.createTestData();
+    const response = await supertest(app)
+      .get('/tests?groupBy=teachers')
+      .set('Authorization', token);
+    expect(response.status).toBe(200);
+    expect(response.body.tests).not.toBe(null);
+  });
+
+  it('given no groupBy should throw error, expect 400', async () => {
+    const token = UserFactory.generateToken();
+    await testFactory.createTestData();
+    const response = await supertest(app)
+      .get('/tests')
+      .set('Authorization', token);
+    expect(response.status).toBe(400);
+    expect(response.body.tests).toBe(undefined);
+  });
+
+  it('missing valid token should throw error, expect 401', async () => {
     const token = 'invalidtoken';
     await testFactory.createTestData();
     const response = await supertest(app)
       .get('/tests?groupBy=disciplines')
       .set('Authorization', token);
     expect(response.status).toBe(401);
-    expect(response.body.tests).toBe(null);
+    expect(response.body.tests).toBe(undefined);
   });
 });
 
