72005cde03ea412a970e1564cdc5a59c15c270a2
Pimenta <mateuspimartins@hotmail.com>
2022-07-25 14:59:57 -0300

    feat: add token validation to routes

--- src/routers/testRouter.ts
+++ src/routers/testRouter.ts
@@ -6,11 +6,17 @@ import {
 } from '../controllers/testController.js';
 import validateSchema from '../middlewares/schemaValidateMiddleware.js';
 import CreateTestSchema from '../models/testSchema.js';
+import validateToken from '../middlewares/tokenValidateMiddleware.js';
 
 const testRouter = Router();
 
-testRouter.post('/tests', validateSchema(CreateTestSchema), createTest);
-testRouter.get('/tests', getTests); // query string groupBy = <teachers|disciplines>
-testRouter.get('/categories', getCategories);
+testRouter.post(
+  '/tests',
+  validateToken,
+  validateSchema(CreateTestSchema),
+  createTest
+);
+testRouter.get('/tests', validateToken, getTests); // query string groupBy = <teachers|disciplines>
+testRouter.get('/categories', validateToken, getCategories);
 
 export default testRouter;
